01a52f8454b16f489c654c70e3b51a5d70da03f2
PandiKumar Dev <pandikumar7498@gmail.com>
2026-07-26 17:53:43 +0530

    bug fixed

--- frontend/src/pages/register-page.jsx
+++ frontend/src/pages/register-page.jsx
@@ -122,7 +122,7 @@ export function RegisterPage() {
             onChange={(value) => update("organizationName", value)}
           />
           <Field
-            label="Organization URL"
+            label="organization Slug "
             name="organizationSlug"
             autoComplete="off"
             value={values.organizationSlug}
